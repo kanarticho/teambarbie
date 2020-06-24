ecbc48a91cfc91045fc355f3932ab78a4ba07392
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-24 21:12:55 +0200

    fixtures medication et mood

---
+++ src/DataFixtures/MedicationFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Medication;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class MedicationFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i=0; $i<=20; $i++) {
+            $medication = new Medication();
+            $medication->setName($faker->realText(50));
+        }
+        $manager->flush();
+    }
+}

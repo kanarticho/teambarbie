af6a6fc6a93eeea8fb1dc2c9ae756da640cb4ff2
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-25 16:42:14 +0200

    ajout add moodday sur home patient

--- src/Controller/HomePatientController.php
+++ src/Controller/HomePatientController.php
@@ -4,7 +4,12 @@
 namespace App\Controller;
 
 
+use App\Entity\Mood;
+use App\Entity\Moodday;
 use App\Entity\Patient;
+use App\Form\MooddayType;
+use App\Repository\MoodRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,8 +19,20 @@ class HomePatientController extends AbstractController
     /**
      * @Route("homePatient/{id}", name="homePatient")
      */
-    public function index(Patient $patient): Response
+    public function index(Patient $patient, MoodRepository $moodRepository, Request $request): Response
     {
-        return $this->render('home/patient.html.twig', ['patient' => $patient]);
+        $moodday = new Moodday();
+        $form = $this->createForm(MooddayType::class, $moodday);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($moodday);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('moodday_index');
+        }
+        return $this->render('home/patient.html.twig', ['patient' => $patient, 'moodday' => $moodday,
+            'form' => $form->createView(), ]);
     }
 }
